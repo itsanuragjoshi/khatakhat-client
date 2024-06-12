a7ba8edac6ce5ca0215c3a85bdd75ab7a8c07d20
itsanuragjoshi <105036156+itsanuragjoshi@users.noreply.github.com>
2024-06-13 00:59:55 +0530

    added collapsible side menu with sub-navigation

---
+++ src/common/components/menu/Menu.jsx
@@ -0,0 +1,111 @@
+import styles from './menu.module.css';
+import Dashboard from '@mui/icons-material/HomeOutlined';
+import Settings from '@mui/icons-material/SettingsOutlined';
+import ArrowBack from '@mui/icons-material/ArrowBackIosNewOutlined';
+import ArrowForward from '@mui/icons-material/ArrowForwardIosOutlined';
+import Logo from '@mui/icons-material/AccountBalanceWalletOutlined';
+import Sell from '@mui/icons-material/SellOutlined';
+
+import MenuButton from './MenuButton';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const navList = [
+  {
+    to: "/dashboard",
+    icon: <Dashboard />,
+    title: "Dashboard",
+  },
+  {
+    icon: <Sell />,
+    title: "Sales",
+    subNavList: [
+      {
+        to: "/customers",
+        title: "Customers",
+      },
+      {
+        to: "/invoices",
+        title: "Invoices",
+      },
+    ],
+  },
+  {
+    icon: <Settings />,
+    title: "Settings",
+    subNavList: [
+      {
+        to: "/settings/orgprofile",
+        title: "Profile",
+      },
+      {
+        to: "/settings/currencies",
+        title: "Currencies",
+      },
+      {
+        to: "/settings/users",
+        title: "Users",
+      },
+    ],
+  },
+];
+
+const Menu = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [openSubNavIndex, setOpenSubNavIndex] = useState(null);
+
+  const toggleMenu = () => {
+    setCollapsed(!collapsed);
+    if (!collapsed) {
+      setOpenSubNavIndex(null); // Close all submenus when the panel is collapsed
+    }
+  };
+
+  const handleSubNav = (index) => {
+    setOpenSubNavIndex(openSubNavIndex === index ? null : index);
+  };
+
+  const expandMenu = () => {
+    setCollapsed(false);
+  };
+
+  return (
+    <menu className={collapsed ? styles.collapsed : undefined}>
+      <div className={`${styles.logoWrapper} ${collapsed ? styles.collapsed : ''}`} title='KhataKhat'>
+        <Link to='/dashboard' onClick={expandMenu}>
+          <Logo />
+          {!collapsed && <span>KhataKhat</span>}
+        </Link>
+      </div>
+
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          {navList.map((item, i) => (
+            <li key={i} title={item.title} className={styles.navItem}>
+              <MenuButton
+                to={item.to}
+                icon={item.icon}
+                title={item.title}
+                subNavList={item.subNavList}
+                collapsed={collapsed}
+                open={openSubNavIndex === i}
+                handleSubNav={() => handleSubNav(i)}
+                expandMenu={expandMenu}
+              />
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <div
+        className={styles.collapseBtnWrapper}
+        title={collapsed ? 'Show Side Panel' : 'Hide Side Panel'}
+        onClick={toggleMenu}
+      >
+        {collapsed ? <ArrowForward /> : <ArrowBack />}
+      </div>
+    </menu>
+  );
+};
+
+export default Menu;
