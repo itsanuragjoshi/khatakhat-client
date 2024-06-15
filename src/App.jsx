5c1af20924b4d3033019952ed07b3a939553944d
itsanuragjoshi <105036156+itsanuragjoshi@users.noreply.github.com>
2024-06-16 00:04:59 +0530

    add routes for customers module

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import Footer from './common/components/footer/Footer'
 import Menu from './common/components/menu/Menu'
+import Customers from './pages/customers/customers'
+import CustomersNew from './pages/customers/customersNew'
 
 function App() {
   return (
     <main className='app'>
       <Menu />
       <div className='contentContainer'>
-        <h1>Home</h1>
+        <Routes>
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/customers/new" element={<CustomersNew />} />
+        </Routes>
         <Footer />
       </div>
     </main>
