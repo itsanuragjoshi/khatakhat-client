5eda03d813c16eca19d71a2fdf2f0689b49290d4
itsanuragjoshi <105036156+itsanuragjoshi@users.noreply.github.com>
2024-06-30 14:20:11 +0530

    added loading spinner

---
+++ src/common/components/loader/Loader.jsx
@@ -0,0 +1,12 @@
+import styles from "./loader.module.css";
+import Spinner from "../../../assets/spinner.svg?react";
+
+const Loader = () => {
+  return (
+    <>
+      <Spinner />
+    </>
+  );
+};
+
+export default Loader;
