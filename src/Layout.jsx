ed9088e3b9473f037e127eb1b90de4d195522466
itsanuragjoshi <105036156+itsanuragjoshi@users.noreply.github.com>
2024-08-02 12:50:51 +0530

    added Suspense

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,9 @@
+import { Suspense } from "react";
 import Toast from "./common/components/toast/Toast";
 import useToastContext from "./common/hooks/useToastContext";
 import Footer from "./common/components/footer/Footer";
 import Menu from "./common/components/menu/Menu";
+import Loader from "./common/components/loader/Loader";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
@@ -12,7 +14,9 @@ function Layout() {
       <Menu />
       <div className="contentContainer">
         <Toast toastList={toastList} />
-        <Outlet />
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
         <Footer />
       </div>
     </main>
