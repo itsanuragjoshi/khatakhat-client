6bfaeacff3fb366ec72e4a88acfcb7a18e7e7c5b
itsanuragjoshi <105036156+itsanuragjoshi@users.noreply.github.com>
2024-08-02 12:51:40 +0530

    changed navigation url

--- src/common/components/wrapper/RequireAuthZ.jsx
+++ src/common/components/wrapper/RequireAuthZ.jsx
@@ -1,27 +1,24 @@
 import { useSelector } from "react-redux";
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Loader from "../loader/Loader";
 
 const RequireAuthZ = ({ children, module, permission }) => {
-  const { orgId } = useParams();
   const { userRoles } = useSelector((state) => state.auth);
   const isLoading = useSelector((state) => state.loading.persistAuth);
 
   // Check if userRoles contains an error
   if (userRoles?.error) {
-    return <Navigate to="/unauthorized" />;
+    return <Navigate to="/error403" />;
   }
-
-  const role = userRoles?.find((userRole) => userRole.orgId._id === orgId);
 
   return isLoading ? (
     <Loader />
-  ) : !role ? (
-    <Navigate to="/unauthorized" />
-  ) : role.roleId.permissions?.[module]?.[permission] ? (
+  ) : !userRoles ? (
+    <Navigate to="/error403" />
+  ) : userRoles?.roleId?.permissions?.[module]?.[permission] ? (
     children
   ) : (
-    <Navigate to="/unauthorized" />
+    <Navigate to="/error403" />
   );
 };
 
