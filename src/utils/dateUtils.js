01fc2b5c38ae914b40d1e0e7509b608a634e009a
itsanuragjoshi <105036156+itsanuragjoshi@users.noreply.github.com>
2024-08-15 13:54:29 +0530

    created a utility function to format a date string

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -2,3 +2,9 @@
 export function getCurrentDate() {
   return new Date().toISOString().split("T")[0];
 }
+
+export function formatDate(dateString) {
+  const date = new Date(dateString);
+  const options = { day: "2-digit", month: "short", year: "numeric" };
+  return date.toLocaleDateString("en-GB", options).replace(/ /g, " ");
+}
